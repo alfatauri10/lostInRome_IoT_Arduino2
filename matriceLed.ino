cdd9af674fe14219c368e64bea3cf37265ef00cd
alfatauri10 <154510711+alfatauri10@users.noreply.github.com>
2025-09-28 19:17:57 +0200

    Add files via upload

---
+++ matriceLed.ino
@@ -0,0 +1,40 @@
+#include "ArduinoGraphics.h"
+#include "Arduino_LED_Matrix.h"
+
+ArduinoLEDMatrix matrix;   // <--- QUESTA QUI
+
+void setup() {
+  Serial.begin(115200);
+  matrix.begin();
+
+  matrix.beginDraw();
+  matrix.stroke(0xFFFFFFFF);
+  // add some static text
+  const char text[] = "";
+  matrix.textFont(Font_4x6);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.println(text);
+  matrix.endText();
+
+  matrix.endDraw();
+
+  delay(2000);
+}
+
+void loop() {
+
+  // Make it scroll!
+  matrix.beginDraw();
+
+  matrix.stroke(0xFFFFFFFF);
+  matrix.textScrollSpeed(50);
+
+  // add the text
+  const char text[] = "    Hello World!    ";
+  matrix.textFont(Font_5x7);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.println(text);
+  matrix.endText(SCROLL_LEFT);
+
+  matrix.endDraw();
+}
